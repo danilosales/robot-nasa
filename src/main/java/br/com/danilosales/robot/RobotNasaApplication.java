ac07a1c330abfd026041e75e28711a61644e7c27
Danilo Sales <danilosales.info@gmail.com>
2019-08-21 08:03:25 -0300

    Start project structure

---
+++ src/main/java/br/com/danilosales/robot/RobotNasaApplication.java
@@ -0,0 +1,13 @@
+package br.com.danilosales.robot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RobotNasaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RobotNasaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/danilosales/robot/RobotNasaApplicationTests.java
@@ -0,0 +1,16 @@
+package br.com.danilosales.robot;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RobotNasaApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
